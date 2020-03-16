7dee81f4c46d563952cd81745487dc1fbe506b9b
Paola L <paolalarrazabal@gmail.com>
2020-03-16 02:13:40 -0400

    removed results.push and removed extra code

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
 const express = require("express");
-let app = express();
 const exphbs = require("express-handlebars")
 
 const bodyParser = require("body-parser")
+const path = require("path");
+//const request = require("request");
 
 const mongojs = require("mongojs");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const axios = require("axios");
-const cheerio = require("cheerio");
 // will move this to app.js require('dotenv').config()
+
+let app = express();
 
 
 app.use(express.static("public")); 
 
 
+
 const PORT = process.env.PORT || 8080;
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-var db = mongoose.connection; 
+var mongooseDB = mongoose.connection; 
 
-db.on("error", console.error.bind(console, "connection error:")); 
-db.once("open", function() {
+mongooseDB.on("error", console.error.bind(console, "connection error:")); 
+mongooseDB.once("open", function() {
 console.log("connected to Mongoose")
 });
 
@@ -33,6 +35,8 @@ app.use(bodyParser.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+require("./routes/controller")(app); 
 
 app.listen(PORT, function() {
     console.log(`App listening on PORT ${PORT} this part works`);
